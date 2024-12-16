a5419d24110f74660c779a025a8a0fd881aa2c4b
Mamba200 <202201253@pua.edu.eg>
2024-12-16 23:15:44 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_yzM34OFzQ', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '4rqk4olm1v55i7tgvt8sbqlvnt', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '3trajt6j8jmmrokocj5krgatpe', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
